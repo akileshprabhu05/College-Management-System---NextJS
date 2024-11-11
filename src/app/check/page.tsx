a8626fc4d3a00b396373d887eca0d5341ae8579a
Abdulgsk <abdul29112004@gmail.com>
2024-11-11 11:29:36 -0800

    auth

---
+++ src/app/check/page.tsx
@@ -0,0 +1,11 @@
+import BigCalendar from "@/Components/BigCalendar";
+
+export default function check() {
+
+    return(
+        <div className="flex h-screen w-full justify-center items-center">
+            <BigCalendar/>
+        </div>
+    )
+
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, SignedIn, SignedOut,  SignInButton, UserButton } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <header className="flex justify-end items-end w-screen pr-4 mt-2">
+        <div>
+        <SignedOut>
+          <SignInButton/>
+        </SignedOut>
+        <SignedIn>
+          <UserButton showName/>
+        </SignedIn>
+        </div>
+      </header>
+        {children}</body>
     </html>
+    </ClerkProvider>
   );
 }
