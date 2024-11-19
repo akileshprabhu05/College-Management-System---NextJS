d9fde80776d4b6959c9469b7524f0e405fa9a1a2
akileshprabhu05 <akileshsakthi05@gmail.com>
2024-11-19 23:27:54 +0530

    clerk

--- src/app/(auth)/sign-in/[[...rest]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function page() {
-    return(
-        <div className="flex h-screen justify-center items-center">
-            <SignIn/>
-        </div>
-    )
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider, SignedIn, SignedOut,  SignInButton, UserButton } from "@clerk/nextjs";
+import { ClerkProvider} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,18 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-      <header className="flex justify-end items-end w-screen pr-4 mt-2">
-        <div>
-        <SignedOut>
-        </SignedOut>
-        <SignedIn>
-          <UserButton showName/>
-        </SignedIn>
-        </div>
-      </header>
-        {children}</body>
+     <html lang="en">
+      <body className={inter.className}>{children}</body>
     </html>
     </ClerkProvider>
   );
